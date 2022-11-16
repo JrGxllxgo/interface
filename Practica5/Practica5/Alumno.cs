8f4f43bb6584167eba6c6bbc38ccc4d8411f31b7
Kacha <jorapijo42@gmail.com>
2022-11-16 13:54:57 +0100

    continuacion practica5

---
+++ Practica5/Practica5/Alumno.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica5
+{
+    internal class Alumno
+    {
+        #region atributos
+        int numMatr, contador = 0;
+        string nombre;
+        List<int> notas;
+        #endregion  
+
+        #region propiedades
+        public int NumMatr
+        {
+            get { return numMatr; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+        }
+        public List<int>Notas
+        {
+            get { return notas; }
+        }
+        #endregion
+        Alumno(string nombre, List<int> notas)
+        {
+            numMatr = contador++;
+            this.nombre = nombre;
+            this.notas = notas;
+        }
+
+        public void imprimeAlumno()
+        {
+            Console.WriteLine("Matrícula: " + NumMatr + "Nombre: " + Nombre);
+        }
+
+        public int mediaAlumno()
+        {
+            int media = 0;
+            for(int i = 0; i < Notas.Count; i++)
+            {
+                media += Notas[i];
+            }
+
+            media = media / Notas.Count;
+            return media;
+        }
+    }
+}
---
+++ Practica5/Practica5/Grupo.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica5
+{
+    internal class Grupo
+    {
+        #region atributos
+        private string nombreGrupo;
+        private int numAsignaturas;
+        private string[] codsignaturas;
+        List<Alumno> alumnos;
+        #endregion
+
+        #region propiedades
+        public string NombreGrupo
+        {
+            get { return nombreGrupo; }
+        }
+        public int NumAsignaturas
+        {
+            get { return numAsignaturas; }
+        }
+        public string[] Codsignaturas
+        {
+            get { return codsignaturas; }
+        }
+        #endregion
+
+        public Grupo(String nombreGrupo, int numAsignaturas, string[] codAsignaturas)
+        {
+            this.nombreGrupo = nombreGrupo;
+            this.numAsignaturas = numAsignaturas;
+            this.codsignaturas = codsignaturas;
+            this.alumnos = new List<Alumno>();
+        }
+
+        public void anadirAlumno(Alumno alumno)
+        {
+            alumnos.Add(alumno);
+        }
+
+        private int localizaAlumno(int matricula)
+        {
+            int pos = -1;
+            for(int i = 0; i < alumnos.Count; i++)
+            {
+                if (alumnos[i].NumMatr == matricula)
+                {
+                    pos = i;
+                }
+            }
+
+            return pos;
+        }
+
+        public Alumno buscaAlumno(int numMatricula)
+        {
+            int pos = localizaAlumno(numMatricula);
+            Alumno encontrado = null;
+            if (pos != -1)
+            {
+                encontrado = alumnos[pos];
+            }
+            return encontrado;
+        }
+
+        public bool borraAlumno(int numMatricula)
+        {
+            int pos = localizaAlumno(numMatricula);
+
+            bool del = false;
+
+            if(pos != 1)
+            {
+                del = true;
+            }
+
+            return del;
+        }
+
+        public double mediaAsignatura(int posicionAs)
+        {
+            int media = 0;
+            for (int i = 0; i < alumnos.Count; i++)
+            {
+                media += alumnos[i].Notas[posicionAs];
+            }
+            media = media / alumnos.Count;
+            return media;
+        }
+
+        public 
+    }
+}
